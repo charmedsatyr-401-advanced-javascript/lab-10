41a238a3410509c252d4f909f93b668b27cd26bf
Joseph <z@charmed.tech>
2019-03-30 10:55:22 -0700

    Rearrange folder structure

--- server.js
+++ server.js
@@ -21,14 +21,16 @@ client.on('error', err => console.error(err));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(methodOverride((request, response) => {
-  if (request.body && typeof request.body === 'object' && '_method' in request.body) {
-    // look in urlencoded POST bodies and delete it
-    let method = request.body._method;
-    delete request.body._method;
-    return method;
-  }
-}))
+app.use(
+  methodOverride((request, response) => {
+    if (request.body && typeof request.body === 'object' && '_method' in request.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = request.body._method;
+      delete request.body._method;
+      return method;
+    }
+  })
+);
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
@@ -52,7 +54,9 @@ function Book(info) {
 
   this.title = info.title ? info.title : 'No title available';
   this.author = info.authors ? info.authors[0] : 'No author available';
-  this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
+  this.isbn = info.industryIdentifiers
+    ? `ISBN_13 ${info.industryIdentifiers[0].identifier}`
+    : 'No ISBN available';
   this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail : placeholderImage;
   this.description = info.description ? info.description : 'No description available';
   this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
@@ -61,12 +65,13 @@ function Book(info) {
 function getBooks(request, response) {
   let SQL = 'SELECT * FROM books;';
 
-  return client.query(SQL)
+  return client
+    .query(SQL)
     .then(results => {
       if (results.rows.rowCount === 0) {
         response.render('pages/searches/new');
       } else {
-        response.render('pages/index', { books: results.rows })
+        response.render('pages/index', { books: results.rows });
       }
     })
     .catch(err => handleError(err, response));
@@ -75,10 +80,15 @@ function getBooks(request, response) {
 function createSearch(request, response) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
 
-  if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
-  if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+  if (request.body.search[1] === 'title') {
+    url += `+intitle:${request.body.search[0]}`;
+  }
+  if (request.body.search[1] === 'author') {
+    url += `+inauthor:${request.body.search[0]}`;
+  }
 
-  superagent.get(url)
+  superagent
+    .get(url)
     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
     .then(results => response.render('pages/searches/show', { results: results }))
     .catch(err => handleError(err, response));
@@ -89,18 +99,18 @@ function newSearch(request, response) {
 }
 
 function getBook(request, response) {
-  getBookshelves()
-    .then(shelves => {
-
-      let SQL = 'SELECT books.*, bookshelves.name FROM books INNER JOIN bookshelves on books.bookshelf_id=bookshelves.id WHERE books.id=$1;';
-      let values = [request.params.id];
-      client.query(SQL, values)
-        .then(result => {
-          console.log(shelves.rows)
-          response.render('pages/books/show', { book: result.rows[0], bookshelves: shelves.rows })
-        })
-        .catch(err => handleError(err, response));
-    })
+  getBookshelves().then(shelves => {
+    let SQL =
+      'SELECT books.*, bookshelves.name FROM books INNER JOIN bookshelves on books.bookshelf_id=bookshelves.id WHERE books.id=$1;';
+    let values = [request.params.id];
+    client
+      .query(SQL, values)
+      .then(result => {
+        console.log(shelves.rows);
+        response.render('pages/books/show', { book: result.rows[0], bookshelves: shelves.rows });
+      })
+      .catch(err => handleError(err, response));
+  });
 }
 
 function getBookshelves() {
@@ -115,35 +125,33 @@ function createShelf(shelf) {
   let SQL1 = `SELECT id from bookshelves where name=$1;`;
   let values1 = [normalizedShelf];
 
-  return client.query(SQL1, values1)
-    .then(results => {
-      if (results.rowCount) {
-        return results.rows[0].id;
-      } else {
-        let INSERT = `INSERT INTO bookshelves(name) VALUES($1) RETURNING id;`;
-        let insertValues = [shelf];
+  return client.query(SQL1, values1).then(results => {
+    if (results.rowCount) {
+      return results.rows[0].id;
+    } else {
+      let INSERT = `INSERT INTO bookshelves(name) VALUES($1) RETURNING id;`;
+      let insertValues = [shelf];
 
-        return client.query(INSERT, insertValues)
-          .then(results => {
-            return results.rows[0].id;
-          })
-      }
-    })
+      return client.query(INSERT, insertValues).then(results => {
+        return results.rows[0].id;
+      });
+    }
+  });
 }
 
 function createBook(request, response) {
   console.log(request.body.bookshelf);
-  createShelf(request.body.bookshelf)
-    .then(id => {
-      let { title, author, isbn, image_url, description } = request.body;
-      let SQL = 'INSERT INTO books(title, author, isbn, image_url, description, bookshelf_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING id;';
-      let values = [title, author, isbn, image_url, description, id];
+  createShelf(request.body.bookshelf).then(id => {
+    let { title, author, isbn, image_url, description } = request.body;
+    let SQL =
+      'INSERT INTO books(title, author, isbn, image_url, description, bookshelf_id) VALUES($1, $2, $3, $4, $5, $6) RETURNING id;';
+    let values = [title, author, isbn, image_url, description, id];
 
-      client.query(SQL, values)
-        .then(result => response.redirect(`/books/${result.rows[0].id}`))
-        .catch(err => handleError(err, response));
-    })
-
+    client
+      .query(SQL, values)
+      .then(result => response.redirect(`/books/${result.rows[0].id}`))
+      .catch(err => handleError(err, response));
+  });
 }
 
 function updateBook(request, response) {
@@ -152,7 +160,8 @@ function updateBook(request, response) {
   let SQL = `UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5, bookshelf_id=$6 WHERE id=$7;`;
   let values = [title, author, isbn, image_url, description, bookshelf_id, request.params.id];
 
-  client.query(SQL, values)
+  client
+    .query(SQL, values)
     .then(response.redirect(`/books/${request.params.id}`))
     .catch(err => handleError(err, response));
 }
@@ -161,7 +170,8 @@ function deleteBook(request, response) {
   let SQL = 'DELETE FROM books WHERE id=$1;';
   let values = [request.params.id];
 
-  return client.query(SQL, values)
+  return client
+    .query(SQL, values)
     .then(response.redirect('/'))
     .catch(err => handleError(err, response));
 }
