708439181d1a982ee766af6192988cfe4f509e55
Joseph <z@charmed.tech>
2019-03-31 00:17:17 -0700

    Remove logging

--- src/middleware/model-finder.js
+++ src/middleware/model-finder.js
@@ -3,9 +3,7 @@
  * Model Finder Middleware
  * @module middleware/model-finder
  */
-
 module.exports = (req, res, next) => {
-  console.log('req.params', req.params);
   const db = process.env.DB === 'SQL' ? 'sql' : 'mongo';
   // const modelName = req.params.model.replace(/[^a-z0-9-_]/gi, '');
   req.model = require(`../models/${db}/books/books-model.js`);
