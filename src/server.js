c48a49e7120021320feda5958c2643a6ca6076d2
Joseph <z@charmed.tech>
2019-03-31 12:56:29 -0700

    Move PG database setup to index.js

--- src/server.js
+++ src/server.js
@@ -13,12 +13,6 @@ app.set('view engine', 'ejs');
 
 app.use(router);
 
-// Database Setup
-const pg = require('pg');
-const client = new pg.Client(process.env.SQL_DATABASE_URL);
-client.connect();
-client.on('error', err => console.error(err));
-
-const start = port => app.listen(port, () => console.log(`Listening on port: ${port}`));
-
-module.exports = { client, start };
+module.exports = {
+  start: port => app.listen(port, () => console.log(`Listening on port: ${port}`)),
+};
