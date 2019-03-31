5410851856baf49ccb4c091affd2ad1dce62aa59
Joseph <z@charmed.tech>
2019-03-30 17:17:37 -0700

    Tweaks

--- src/middleware/model-finder.js
+++ src/middleware/model-finder.js
@@ -5,8 +5,9 @@
  */
 
 module.exports = (req, res, next) => {
+  console.log('req.params', req.params);
   const db = process.env.DB === 'SQL' ? 'sql' : 'mongo';
-  // let modelName = req.params.model.replace(/[^a-z0-9-_]/gi, '');
+  // const modelName = req.params.model.replace(/[^a-z0-9-_]/gi, '');
   req.model = require(`../models/${db}/books/books-model.js`);
   next();
 };
