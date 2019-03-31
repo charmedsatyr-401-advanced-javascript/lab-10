3ed237083f5ea4adc182401f2293fd9f95d57a05
Joseph <z@charmed.tech>
2019-03-31 14:45:34 -0700

    Logs whether DB is PG or Mongo

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ if (process.env.DB === 'SQL') {
   console.log();
   client = new pg.Client(process.env.SQL_DATABASE_URL);
   client.connect(err =>
-    err ? console.log(`Connection error: ${err.stack}`) : console.log(`Connected to PostgresSQL`)
+    err
+      ? console.log(`Connection error: ${err.stack}`)
+      : console.log(`Connected to PostgresSQL database.`)
   );
   client.on('error', err => console.error(err));
 } else if (process.env.DB === 'MONGO') {
@@ -19,7 +21,9 @@ if (process.env.DB === 'SQL') {
     useNewUrlParser: true,
     useCreateIndex: true,
   };
-  mongoose.connect(process.env.MONGO_URI, mongooseOptions);
+  mongoose.connect(process.env.MONGO_URI, mongooseOptions, err =>
+    err ? console.log(`Connection error: ${err}`) : console.log(`Connected to MongoDB.`)
+  );
 }
 
 const PORT = process.env.PORT || 3000;
