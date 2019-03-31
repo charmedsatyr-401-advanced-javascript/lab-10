f4183bf1e2976f41e068f87093b295b4129e689b
Joseph <z@charmed.tech>
2019-03-30 17:17:55 -0700

    Remove unnecessary PORT; move pg require

--- src/server.js
+++ src/server.js
@@ -4,11 +4,9 @@ const router = require('./api/v1.js');
 
 // Application Dependencies
 const express = require('express');
-const pg = require('pg');
 
 // Application Setup
 const app = express();
-const PORT = process.env.PORT;
 
 // Set the view engine for server-side templating
 app.set('view engine', 'ejs');
@@ -16,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(router);
 
 // Database Setup
+const pg = require('pg');
 const client = new pg.Client(process.env.SQL_DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
